f370136b522dc9bb676b431ed5d85df53a0e83c4
vipin <vipinkmundra@gmail.com>
2023-02-25 13:56:48 +0530

    solution1

---
+++ src/main/java/com/driver/model/Cab.java
@@ -0,0 +1,53 @@
+package com.driver.model;
+
+import javax.persistence.*;
+import com.driver.model.Driver;
+
+@Entity
+@Table(name = "cab")
+public class Cab{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int perKmRate;
+    private boolean available;
+
+    @OneToOne
+    @JoinColumn
+    private Driver driver;
+
+    public Cab() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
